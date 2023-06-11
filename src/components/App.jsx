72b02e74b7a6b19e23ee4eaedcac676f1fa943b5
Aleks-TO <satov69@gmail.com>
2023-06-11 18:14:02 +0300

    copy last homework

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,73 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import { nanoid } from 'nanoid';
+import css from './app.module.css';
+import ContactForm from './contactForm/contactForm';
+import ContactList from './contactList/contactList';
+import Filter from './filter/filter';
+
+class App extends Component {
+  state = {
+    contacts: [],
+    filter: '',
+  };
+
+  formSubmit = event => {
+    event.preventDefault();
+    const { contacts } = this.state;
+    const { name, number } = event.target;
+    // console.log(event.target);
+    if (contacts.map(contact => contact.name).includes(name.value)) {
+      return alert(`Name ${name.value} is already here`);
+    }
+
+    this.setState({
+      contacts: [
+        ...contacts,
+        {
+          id: nanoid(),
+          name: name.value,
+          number: number.value,
+        },
+      ],
+    });
+    event.currentTarget.reset();
+  };
+
+  deleteContact = event => {
+    const { contacts } = this.state;
+    const { id } = event.currentTarget.parentElement;
+
+    this.setState({
+      contacts: contacts.filter(contact => contact.id !== id),
+    });
+  };
+
+  handleInputChange = event => {
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value,
+    });
+  };
+
+  render() {
+    return (
+      <div className={css.phonebook}>
+        <h1 className={css.phonebook__title}>Phonebook</h1>
+        <ContactForm state={this.state} formSubmit={this.formSubmit} />
+        <h2 className={css.phonebook__contactsTitle}>Contacts</h2>
+        <Filter
+          contacts={this.state.contacts}
+          filter={this.state.filter}
+          handleInputChange={this.handleInputChange}
+        />
+        <ContactList
+          contacts={this.state.contacts}
+          filter={this.state.filter}
+          onDeleteContact={this.deleteContact}
+        />
+      </div>
+    );
+  }
+}
+
+export default App;
